36f6c4ceffb10008067d54f6aa6fc3e471fc3d78
Sooraj <soorajsunil38@gmail.com>
2026-04-22 16:15:19 +0530

    Updated login and signup integration

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LandingPage from './Pages/LandingPage.jsx'
+import Login from './auth/Login.jsx'
+import SignUp from './auth/SignUp.jsx'
+import favicon from './assets/favicon.png'
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Routes>
+      </div>
+    </Router>
   )
 }
 
